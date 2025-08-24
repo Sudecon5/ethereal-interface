3f8f2785b45990066a0cc737cc6c0242b8311aff
Sudecon5 <sudiptapriyam55@gmail.com>
2025-08-24 23:47:10 +0200

    Backend  commit

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(bodyParser.json());
